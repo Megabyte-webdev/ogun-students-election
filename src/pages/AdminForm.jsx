06afc09ca40368873bf521df38166560291eec22
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2026-01-28 06:29:28 +0100

    Update AdminForm.jsx

--- src/pages/AdminForm.jsx
+++ src/pages/AdminForm.jsx
@@ -36,7 +36,7 @@ export default function AdminLogin() {
     setStatus({ loading: true, error: "", success: "" });
 
     try {
-      const res = await fetch(`http://localhost:5000/api/admin/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/admin/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
